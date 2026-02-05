dlyongemallo/diffview.nvim

commit 1b386ce4715ba0fd3493ba188b48bdd07fef8393

feat: add close keymaps (q, <esc>) to commit log panel (#482)
Add q and <esc> keymaps to close the commit log panel, matching the
behaviour of other panels like the help panel. The keymaps are
configurable via keymaps.commit_log_panel.

Fixes #481

lua/diffview/ui/panels/commit_log_panel.lua
@@ -51,7 +51,7 @@ end
 
 ---@param adapter VCSAdapter
 ---@param opt CommitLogPanelSpec
-function CommitLogPanel:init(adapter, opt)
+function CommitLogPanel:init(parent, adapter, opt)
   self:super({
     bufname = opt.name,
     config = opt.config or get_user_config().commit_log_panel.win_config,
@@ -65,6 +65,21 @@ function CommitLogPanel:init(adapter, opt)
       vim.bo[self.bufid].bufhidden = "wipe"
     end,
   })
+
+  local conf = get_user_config().keymaps
+  local default_opt = { silent = true, nowait = true, buffer = self.bufid }
+
+  for _, mapping in ipairs(conf.commit_log_panel) do
+    local map_opt = vim.tbl_extend("force", default_opt, mapping[4] or {}, { buffer = self.bufid })
+    vim.keymap.set(mapping[1], mapping[2], mapping[3], map_opt)
+  end
+
+  parent.emitter:on("close", function(e)
+    if self:is_focused() then
+      self:close()
+      e:stop_propagation()
+    end
+  end)
 end
 
 ---@param self CommitLogPanel

lua/diffview/config.lua
@@ -255,6 +255,10 @@ M.defaults = {
       { "n", "q",     actions.close,  { desc = "Close help menu" } },
       { "n", "<esc>", actions.close,  { desc = "Close help menu" } },
     },
+    commit_log_panel = {
+      { "n", "q",     actions.close,  { desc = "Close commit log" } },
+      { "n", "<esc>", actions.close,  { desc = "Close commit log" } },
+    },
   },
 }
 -- stylua: ignore end

lua/diffview/scene/views/diff/diff_view.lua
@@ -82,7 +82,7 @@ end
 function DiffView:post_open()
   vim.cmd("redraw")
 
-  self.commit_log_panel = CommitLogPanel(self.adapter, {
+  self.commit_log_panel = CommitLogPanel(self, self.adapter, {
     name = fmt("diffview://%s/log/%d/%s", self.adapter.ctx.dir, self.tabpage, "commit_log"),
   })
 

lua/diffview/scene/views/file_history/file_history_view.lua
@@ -40,7 +40,7 @@ function FileHistoryView:init(opt)
 end
 
 function FileHistoryView:post_open()
-  self.commit_log_panel = CommitLogPanel(self.adapter, {
+  self.commit_log_panel = CommitLogPanel(self, self.adapter, {
     name = ("diffview://%s/log/%d/%s"):format(self.adapter.ctx.dir, self.tabpage, "commit_log"),
   })
 

commit a9f85991c1b163a1a7d28a7e8b17f4a8a225b192

fix(keymaps): register commit_log_panel keymaps in init_buffer instead of init
Fixes bug introduced in 1b386ce which caused Esc to close any diffview window.

lua/diffview/ui/panels/commit_log_panel.lua
@@ -67,6 +67,17 @@ function CommitLogPanel:init(parent, adapter, opt)
     end,
   })
 
+  parent.emitter:on("close", function(e)
+    if self:is_focused() then
+      self:close()
+      e:stop_propagation()
+    end
+  end)
+end
+
+function CommitLogPanel:init_buffer()
+  CommitLogPanel:super_class().init_buffer(self)
+
   local conf = get_user_config().keymaps
   local default_opt = { silent = true, nowait = true, buffer = self.bufid }
 
@@ -74,13 +85,6 @@ function CommitLogPanel:init(parent, adapter, opt)
     local map_opt = vim.tbl_extend("force", default_opt, mapping[4] or {}, { buffer = self.bufid })
     vim.keymap.set(mapping[1], mapping[2], mapping[3], map_opt)
   end
-
-  parent.emitter:on("close", function(e)
-    if self:is_focused() then
-      self:close()
-      e:stop_propagation()
-    end
-  end)
 end
 
 ---@param self CommitLogPanel